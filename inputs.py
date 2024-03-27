81000268a25864bd23c9c95135d837d04e8ac68a
Philipp-BH <157858376+Philipp-BH@users.noreply.github.com>
2024-03-27 15:21:56 -0700

    added potentiometer class for input

--- inputs.py
+++ inputs.py
@@ -1,4 +1,6 @@
 import digitalio
+import analogio
+import extramath
 
 class sw:
     def __init__(self, pin): #pin must be board.GP##
@@ -7,3 +9,19 @@ class sw:
         
     def read(self):
         return self.sw.value
+    
+class pot:
+    def __init__(self, pin): #pin must be board.GP##
+        self.pot = analogio.AnalogIn(pin)
+        
+    def read_u16(self):
+        return self.pot.value
+    
+    def percent(self):
+        return extramath.Map(self.pot.value, 0, 65535, 0, 100)
+    
+    def unitInterval(self):
+        return self.pot.value / 65535
+    
+    def joy(self):
+        return (self.pot.value / 32767.5) - 1

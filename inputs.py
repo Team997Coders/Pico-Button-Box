d85fc1e576ce3876ff11e36aca4fe5da1f9f86b9
Philipp-BH <157858376+Philipp-BH@users.noreply.github.com>
2024-03-27 15:09:25 -0700

    started circuitpython

--- inputs.py
+++ inputs.py
@@ -1,27 +1,9 @@
-from machine import Pin, ADC
-import thread
-
-import extramath
+import digitalio
 
 class sw:
-    def __init__(self, pin):
-        self.sw = Pin(pin, Pin.IN, Pin.PULL_DOWN)
-    
-    def get_value(self): #might be better as lambda?
-        return self.sw.value()
-    
-class pot:
-    def __init__(self, pin):
-        self.pot = ADC(pin)
+    def __init__(self, pin): #pin must be board.GP##
+        self.sw = digitalio.DigitalInOut(pin)
+        self.sw.switch_to_input(pull=digitalio.Pull.DOWN)
         
-    def read_u16(self): #might be better as lambda?
-        return self.pot.read_u16()
-    
-    def read_percent(self): #might be better as lambda?
-        return extramath.Map(self.pot.read_u16(), 0, 65535, 0, 100)
-    
-    def read(self): #might be better as lambda?
-        return self.pot.read_u16() / 65535
-    
-    def read_joy(self): #might be better as lambda?
-        return (self.pot.read_u16() / 32,767.5) - 1
+    def read(self):
+        return self.sw.value
--- outputs.py
+++ outputs.py
@@ -1,50 +1,12 @@
-from machine import Pin, PWM
-import neopixel
-
-import extramath
+import digitalio
 
 class led:
-    def __init__(self, pin):
-        self.led = Pin(pin, Pin.OUT)
+    def __init__(self, pin): #pin must be board.GP##
+        self.led = digitalio.DigitalInOut(pin)
+        self.led = digitalio.Direction.OUTPUT
         
     def on(self):
-        try:
-            PWM.deinit(self.led)
-        except:
-            print("led already not PWM object")
-        self.led.value(1)
+        self.led.value = True
         
     def off(self):
-        try:
-            PWM.deinit(self.led)
-        except:
-            print("led already not PWM object")
-        self.led.value(0)
-        
-    def toggle(self):
-        try:
-            PWM.deinit(self.led)
-        except:
-            print("led already not PWM object")
-        self.led.toggle()
-    
-    def brightness(self, percent):
-        try:
-            self.led = PWM(self.led)
-            self.led.freq(16000)
-        except:
-            print("led already PWM object")
-        
-        self.led.duty_u16(extramath.Map(percent, 0, 100, 0, 65535))
-        
-class neopixel:
-    def __init__(self, pin, len):
-        self.neopixel = neopixel.NeoPixel(Pin(pin), len)
-        
-    def fill(self, rgb):
-        self.neopixel.fill(rgb)
-        self.neopixel.write()
-        
-    def set(self, index, rgb):
-        self.neopixel.__setitem__(index, rgb)
-        self.neopixel.write()
+        self.led.value = False

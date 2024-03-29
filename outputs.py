022a4d874f96ca340fdcdaf910f38f409443d637
Philipp-BH <157858376+Philipp-BH@users.noreply.github.com>
2024-03-29 12:35:17 -0700

    updated to using pwm for LED to allow brightness control

--- outputs.py
+++ outputs.py
@@ -1,12 +1,19 @@
 import digitalio
+import pwmio
+import extramath
 
 class led:
     def __init__(self, pin): #pin must be board.GP##
-        self.led = digitalio.DigitalInOut(pin)
-        self.led = digitalio.Direction.OUTPUT
+        self.led = pwmio.PWMOut(pin, frequency=1000)
         
     def on(self):
-        self.led.value = True
+        self.led.duty_cycle = 65535
         
     def off(self):
-        self.led.value = False
+        self.led.duty_cycle = 0
+    
+    def duty(self, duty):
+        self.led.duty_cycle = duty
+    
+    def percent(self, percent):
+        self.led.duty_cycle = extramath.Map(percent, 0, 100, 0, 65535)

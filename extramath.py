d6b90212372380b564609bb33043123b3eb1404b
Philipp-BH <157858376+Philipp-BH@users.noreply.github.com>
2024-04-02 18:06:56 -0700

    updated with pot trim (#8)
    git push

--- extramath.py
+++ extramath.py
@@ -1,2 +1,9 @@
 def Map(x, in_min, in_max, out_min, out_max): #map one range to another
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+def clamp(num, lower, upper): #limit number to range
+    if num < lower:
+        num = lower
+    elif num > upper:
+        num = upper
+    return num
--- inputs.py
+++ inputs.py
@@ -1,6 +1,6 @@
 import digitalio
 import analogio
-import extramath
+import extramath as umath
 
 class sw: #confirmed as fully working
     def __init__(self, pin): #pin must be board.GP##
@@ -11,22 +11,28 @@ class sw: #confirmed as fully working
         return self.sw.value
     
 class pot: #IN TESTING PROCESS - NONFUNCTIONAL
-    def __init__(self, pin): #pin must be board.GP##
+    def __init__(self, pin, trim_min=0, trim_max=65535): #pin must be board.GP##
+        self.trim_min = trim_min
+        self.trim_max = trim_max
+        
         self.pot = analogio.AnalogIn(pin)
         
-    def read_u16(self): #confirmed as working
-        return self.pot.value
+        self.applyTrim = lambda inputVal: umath.Map(inputVal, self.trim_min, self.trim_max, 0, 65535)
+        self.clamp_u16 = lambda inputVal: umath.clamp(inputVal, 0, 65535)
+        
+    def read_u16(self): 
+        return self.clamp_u16(self.applyTrim(self.pot.value))
     
-    def percent(self, rounding=False): #confirmed as working
-        self.value = extramath.Map(self.pot.value, 0, 65535, 0, 100)
+    def percent(self, rounding=False): 
+        self.value = umath.clamp(umath.Map(self.pot.value, self.trim_min, self.trim_max, 0, 100), 0, 100)
         
         if rounding:
             self.value = round(self.value)
             
         return self.value
     
-    def unitInterval(self): #confirmed as working
-        return self.pot.value / 65535
+    def unitInterval(self): 
+        return umath.clamp(umath.Map(self.pot.value, self.trim_min, self.trim_max, 0, 1), 0, 1)
     
-    def joy(self): #confirmed as working
-        return (self.pot.value / 32767.5) - 1
+    def joy(self): 
+        return umath.clamp(umath.Map(self.pot.value, self.trim_min, self.trim_max, 0, 2) - 1, -1, 1)

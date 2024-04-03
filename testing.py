51b097fd25de2a98dd7ea0f0cdea73e99ce61075
Philipp-BH <157858376+Philipp-BH@users.noreply.github.com>
2024-04-02 18:06:19 -0700

    updated with pot trim git push

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ import inputs
 import board
 import time
 
-potentiometer = inputs.pot(board.GP26)
+potentiometer = inputs.pot(board.GP26, trim_min=530, trim_max=65535)
 
 while True:
     print(potentiometer.joy())
